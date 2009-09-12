06dbc3d98982ed4d480a75b2931cddbb1b63b5ff
Andrew Wason <rectalogic@rectalogic.com>
2009-09-12 19:16:37 -0400

    Reorganize for service.

---
+++ iTunesNotifierService.m
@@ -0,0 +1,16 @@
+//
+//  iTunesNotifierAction.m
+//  iTunesNotifier
+//
+//  Copyright Andrew Wason 2009. All rights reserved.
+//
+
+#import <Vermilion/Vermilion.h>
+
+@interface iTunesNotifierService : HGSExtension
+@end
+
+@implementation  iTunesNotifierService
+
+
+@end
